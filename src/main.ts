136c3eced6903d787348bb3d5de9026c2fe4195b
Sants <106097059+CardonaSantos@users.noreply.github.com>
2026-01-16 10:06:39 -0600

    ajustes main ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // comentario
   app.enableCors({
     origin: [
-      'https://nova-pos-erp.up.railway.app',
+      'https://mini-market-crucero.up.railway.app',
       'http://localhost:5174',
       'http://localhost:5173',
     ],
